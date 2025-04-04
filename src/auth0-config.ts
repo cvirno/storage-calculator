53a0866fe031b2a7cbb1c9cdee070e45ef6c760c
cvirno <cesar.virno@gmail.com>
2025-04-03 21:01:05 -0300

    fix: atualização das URLs de callback do Auth0

--- src/auth0-config.ts
+++ src/auth0-config.ts
@@ -2,6 +2,8 @@ export const auth0Config = {
   domain: "dev-0afpcj54klh2vplm.us.auth0.com",
   clientId: "I14u5aeRr4kvPlcLHxhe5mrADSdT8J55",
   authorizationParams: {
-    redirect_uri: window.location.origin
+    redirect_uri: process.env.NODE_ENV === 'development' 
+      ? 'http://localhost:5173'
+      : 'https://prismatic-choux-897487.netlify.app'
   }
 }; 
