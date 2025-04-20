3f4d225def45893d5aed6397771edeff05221c83
cvirno <cesar.virno@gmail.com>
2025-04-20 10:59:01 -0300

    fix: corrigindo configurações para build no Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,23 @@ export default defineConfig({
     host: '0.0.0.0',
     open: true,
     cors: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ui: ['@radix-ui/react-label', '@radix-ui/react-progress', '@radix-ui/react-select', '@radix-ui/react-slot'],
+          charts: ['recharts']
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 });
